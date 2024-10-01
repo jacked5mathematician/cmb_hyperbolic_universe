f36ce668c25b1fb313bd5e6e037f2db281b2ec03
sexymathematician <ercetin.utku@gmail.com>
2024-10-01 16:19:15 +0200

    Updated initialization

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,6 @@
-# Importing functions or classes from domain_building.py
+# This file is used to import all the functions from the different modules in the utils package
 from .domain_building import convert_to_4x4_matrices, build_dirichlet_domain, generate_random_points_in_domain, filter_points_in_domain, select_points
-
-# Importing functions or classes from special_functions.py
 from .special_functions import Phi_nu_l, normalization_constant, Y_lm_real, Phi_nu_l_cached, Y_lm_real_cached, Q_k_lm, Q_k_lm_cached
-
-# Importing functions or classes from transformations.py
 from .transformations import apply_so31_action, project_to_klein, klein_to_pseudo_spherical, generate_transformed_points, convert_to_points_images
-
 from .svd import solve_system_via_svd_numeric, plot_chi_squared_spectrum
+from .transcendental_solver import find_rho_solution, transcendental_eq
