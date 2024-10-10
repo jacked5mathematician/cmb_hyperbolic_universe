4c1ddff805664f5be47b4a6630edc2cc316beee3
sexymathematician <ercetin.utku@gmail.com>
2024-10-10 16:11:31 +0200

    Updated init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,7 @@
 # This file is used to import all the functions from the different modules in the utils package
 from .domain_building import convert_to_4x4_matrices, build_dirichlet_domain, generate_random_points_in_domain, filter_points_in_domain, select_points
-from .special_functions import Phi_nu_l, normalization_constant, Y_lm_real, Phi_nu_l_cached, Y_lm_real_cached, Q_k_lm, Q_k_lm_cached
+from .special_functions import Phi_nu_l, normalization_constant, Y_lm_real, Phi_nu_l_cached, Y_lm_real_cached, Q_k_lm, parallel_Q_k_lm_compute
 from .transformations import apply_so31_action, project_to_klein, klein_to_pseudo_spherical, generate_transformed_points, convert_to_points_images
 from .svd import solve_system_via_svd_numeric, plot_chi_squared_spectrum
-from .transcendental_solver import find_rho_solution, transcendental_eq
 from .sys_generation import ensure_picklable, compute_column, generate_matrix_system, construct_numeric_matrix
 from .parameter_control import compute_target_M, filter_points_for_overconstraint, select_points_for_c, determine_tiling_radius
